dd531e0af6fda9ee11e800b23479766c3304f0b7
sfertman <sfertman@zoho.com>
2023-09-26 13:40:59 -0400

    rm target url (twin api change) (#6)

--- test/client.test.js
+++ test/client.test.js
@@ -13,7 +13,6 @@ const paywall = {
     url: "https://41d83ecbac7b2a50e451ee2a453fb8f4.tq.biz.todaq.net",
     address: "41d83ecbac7b2a50e451ee2a453fb8f46a32fa071c9fab08f0d597eed3d0e74a0e",
     config: {
-        targetUrl: "https://example.com",
         targetPayType: "41f88b1490292e22ac37a5da7d9cdb88cffda408ae12a188243ad209e6f9fa5ef9",
         targetPayQuantity: 1
     }
