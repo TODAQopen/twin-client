464464bcbbfa3fc5a43511182d99954314048a61
sfertman <sfertman@zoho.com>
2023-10-24 13:14:44 -0400

    the classname thing

--- twin-client-py/client/error.py
+++ twin-client-py/client/error.py
@@ -1,26 +1,25 @@
 class TwinError(Exception):
-  def __init__(self, message="TwinError", data=None):
-    self.message = message
+  def __init__(self, message=None, data=None):
+    self.message = message if message else self.__class__.__name__
     self.data = data
     super().__init__(self.message)
 
 class TwinAuthError(TwinError):
-  def __init__(self, message="TwinAuthError", data=None):
+  def __init__(self, message=None, data=None):
     super().__init__(message, data)
 
 class TwinMicropayError(TwinError):
-  def __init__(self, message="TwinMicropayError", data=None):
+  def __init__(self, message=None, data=None):
     super().__init__(message, data)
 
   @staticmethod
   def from_twin_error(err):
     return TwinMicropayError(err.message, err.data)
 
-
 class TwinMicropayAmountMismatchError(TwinMicropayError):
-  def __init__(self, message="TwinMicropayAmountMismatchError", data=None):
+  def __init__(self, message=None, data=None):
     super().__init__(message, data)
 
 class TwinMicropayTokenMismatchError(TwinMicropayError):
-  def __init__(self, message="TwinMicropayTokenMismatchError", data=None):
+  def __init__(self, message=None, data=None):
     super().__init__(message, data)
