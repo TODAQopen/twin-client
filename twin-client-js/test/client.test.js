104cb48cd13db506128d23dc5188d4500e4468ea
sfertman <sfertman@zoho.com>
2023-12-05 10:13:22 -0500

    test config from file

--- twin-client-js/test/client.test.js
+++ twin-client-js/test/client.test.js
@@ -8,23 +8,10 @@ import {
     TwinMicropayError,
     TwinMicropayAmountMismatchError,
     TwinMicropayTokenMismatchError } from "../src/error.js";
-
 import nock from "nock"
+import testConfig from "./config.js";
 
-const paywall = {
-    url: "https://41d83ecbac7b2a50e451ee2a453fb8f4.tq.biz.todaq.net",
-    address: "41d83ecbac7b2a50e451ee2a453fb8f46a32fa071c9fab08f0d597eed3d0e74a0e",
-    apiKey: "8c0b7fb3-c832-4c54-9f8f-3a5e8eef4e52",
-    config: {
-        targetPayType: "41f88b1490292e22ac37a5da7d9cdb88cffda408ae12a188243ad209e6f9fa5ef9",
-        targetPayQuantity: 1
-    }
-};
-
-const payer = {
-    url: "https://4112873c42e819316dcfafdddb95a5cf.tq.biz.todaq.net",
-    apiKey: "41b95538-b2a5-4aea-9121-a7d4e8558a63"
-};
+const { payer, paywall } = testConfig;
 
 describe("TwinError", async function() {
     it("Should throw TwinError when error is not handled", async function() {
