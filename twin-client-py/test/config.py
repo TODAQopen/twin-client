e22a340f349e28c6f2813bbca40c527d4a6b5f08
sfertman <sfertman@zoho.com>
2023-12-14 10:38:08 -0500

    test config from file (#17)

---
+++ twin-client-py/test/config.py
@@ -0,0 +1,15 @@
+test_config = {
+  'paywall': {
+    'url': "https://41678ba2d72dc94b1ba816b93bc98045.tq.biz.todaq.net",
+    'address': "41678ba2d72dc94b1ba816b93bc98045a2dee5af6982c75685e1f1b57c19dcac29",
+    'api_key': "f9ee1ee8-2966-4ed9-b3a7-3dc6dc9a7fc9",
+    'config': {
+      'targetPayType': "41796848130c3b7527d2bcf289ff3504ebc105f4976741c3b8af65f02db43a81ee",
+      'targetPayQuantity': 1
+    }
+  },
+  'payer': {
+    'url': "https://410ff3ca7c44caa0716cf831cda444e2.tq.biz.todaq.net",
+    'api_key': "fff53e8c-bb79-4662-a3c7-96ee294c13e6"
+  }
+}
